03719c3f896a3c6c54c22311032d02b01b55ef40
Cassandra Brown <clb@uvic.ca>
2014-10-09 23:27:01 -0700

    Rds files created for each year

--- Scripts/Data/Import_sav.R
+++ Scripts/Data/Import_sav.R
@@ -9,8 +9,11 @@ pathFile1RDS <- file.path(pathFolder, "h04f1a.Rds")
 pathFile2 <- file.path(pathFolderSPSS, "h06f2b.sav")
 pathFile2RDS <- file.path(pathFolder, "h06f2b.Rds")
 
-#pathFile3 <- file.path(pathFolderSPSS, "h08f2a.sav")
-#pathFile3RDS <- file.path(pathFolder, "h08f2a.Rds")
+pathFile3 <- file.path(pathFolderSPSS, "h08f2a.sav")
+pathFile3RDS <- file.path(pathFolder, "h08f2a.Rds")
+
+pathFile4 <- file.path(pathFolderSPSS, "h10f4a.sav")
+pathFile4RDS <- file.path(pathFolder, "h10f4a.Rds")
 
 require(foreign)
 # Next three lines are run the first time on machine
@@ -18,6 +21,25 @@ require(foreign)
 # ds0_1 <- data.frame(ds0_1)
 # saveRDS(object = ds0_1, file=pathFile1RDS, compress="xz")
 ds01 <- readRDS(pathFile1RDS)
+
+#Next three lines are run the first time on machine
+#ds0_2 <- read.spss(file=pathFile2, use.value.labels=TRUE)
+#ds0_2 <- data.frame(ds0_2)
+#saveRDS(object = ds0_2, file=pathFile2RDS, compress="xz")
+ds02 <- readRDS(pathFile2RDS)
+
+#Next three lines are run the first time on machine
+#ds0_3 <- read.spss(file=pathFile3, use.value.labels=TRUE)
+#ds0_3 <- data.frame(ds0_3)
+#saveRDS(object = ds0_3, file=pathFile3RDS, compress="xz")
+ds03 <- readRDS(pathFile3RDS)
+
+#Next three lines are run the first time on machine
+#ds0_4 <- read.spss(file=pathFile4, use.value.labels=TRUE)
+#ds0_4 <- data.frame(ds0_4)
+#saveRDS(object = ds0_4, file=pathFile4RDS, compress="xz")
+ds04 <- readRDS(pathFile4RDS)
+
 
 require(dplyr)
 
@@ -34,10 +56,4 @@ ds <- ds01[ 1:200
            
 
 
-# ds0_2 <- read.spss(file=pathFile2, use.value.labels=TRUE)
-# saveRDS(object = ds0_2, file=pathFile2RDS, compress="xz")
-# ds02 <- readRDS(pathFile2RDS)
 
-#ds0_3 <- read.spss(file=pathFile3, use.value.labels=TRUE)
-#saveRDS(object = ds0_3, file=pathFile3RDS, compress="xz")
-#ds03 <- readRDS(pathFile3RDS)
